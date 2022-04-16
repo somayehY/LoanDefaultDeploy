afd1a41e69dd4803b7ddf52b17a8092e4749537b
Somayeh <syoussefi600@gmail.com>
2022-04-15 21:42:47 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 import streamlit as st
 import pandas as pd
 import pickle
+st.write("""
+# Loan Default Prediction App
+This app predicts if a loan defaults or not!
+""")
 
-
-def predict():
-    ir = st.sidebar.slider('int_rate', 3,100,15)
+st.sidebar.header('User Input Features')
+def input_data():
+    ir = st.sidebar.slider('int_rate', 3, 100, 15)
     la = st.sidebar.slider('loan_amount', 1000, 40000, 5000)
     fs = st.sidebar.slider('fico_score', 150, 900, 650)
-    term = st.sidebar.selectbox('term',(' 36 months', ' 60 months'))
+    term = st.sidebar.selectbox('term', (' 36 months', ' 60 months'))
     data = {'int_rate': ir,
             'loan_amnt': la,
             'fico_score': fs,
             'term': term}
     df = pd.DataFrame(data, columns=['int_rate', 'loan_amnt', 'fico_score', 'term'], index=[0])
+    return df
+
+def predict_point(df):
     model = pickle.load(open('model.pkl', 'rb'))
-
     result = model.predict(df)
-
     return result
-result = predict()
+df = input_data()
+response = predict_point(df)
 st.subheader('Prediction')
-
-st.write(result)
+st.write(response)
 
